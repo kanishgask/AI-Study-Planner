2b397e573893949fc25b316a33a34bed2ed141a9
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-04-21 09:40:16 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    String name;
+    int difficulty;
+    int daysLeft;
+
+    public Task(String name, int difficulty, int daysLeft) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.daysLeft = daysLeft;
+    }
+}
